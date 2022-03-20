915310a452e5104fe722d13376ed5b4b4a0bc4ee
codepanther <pezechigozirim@gmail.com>
2022-03-20 17:49:51 +0100

    Create and style CategoryItem component

---
+++ src/components/CategoryItem.js
@@ -0,0 +1,15 @@
+import './categories.styles.scss';
+
+const CategoryItem = ({ category: { title, imageUrl} }) => {
+  return (
+    <div className="category-container">
+      <div className="background-image" style={{ backgroundImage: `url(${imageUrl})`}} />
+      <div className="category-body-container">
+        <h1>{title}</h1>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  )
+}
+
+export default CategoryItem
