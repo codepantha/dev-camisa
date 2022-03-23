6135b03af218f1508761028ea48a0941f1d81c66
codepanther <pezechigozirim@gmail.com>
2022-03-23 19:21:17 +0100

    Set up routing and navigation

--- src/App.js
+++ src/App.js
@@ -1,37 +1,18 @@
-import { Directory } from './components';
+import { Routes, Route } from 'react-router-dom';
+import Home from './routes/Home/Home';
+import Navigation from './routes/Navigation/Navigation';
 
-const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-    },
-    {
-      id: 2,
-      title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-    },
-    {
-      id: 3,
-      title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-    },
-    {
-      id: 4,
-      title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-    },
-    {
-      id: 5,
-      title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-    },
-  ];
+const Shop = () => (
+  <h1>Hi, I&apos;m the Shop page</h1>
+);
 
-  return (
-    <Directory categories={categories} />
-  );
-};
+const App = () => (
+  <Routes>
+    <Route path="/" element={<Navigation />}>
+      <Route index element={<Home />} />
+      <Route path="shop" element={<Shop />} />
+    </Route>
+  </Routes>
+);
 
 export default App;
---
+++ src/routes/Navigation/Navigation.js
@@ -0,0 +1,21 @@
+import { Link, Outlet } from 'react-router-dom';
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+import './Navigation.scss';
+
+const Navigation = () => (
+  <>
+    <nav className="navigation">
+      <Link to="/" className="logo-container">
+        <Logo className="logo" />
+      </Link>
+      <div className="nav-links-container">
+        <Link to="/shop" className="nav-link">
+          SHOP
+        </Link>
+      </div>
+    </nav>
+    <Outlet />
+  </>
+);
+
+export default Navigation;
