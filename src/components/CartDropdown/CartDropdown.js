9b1966a59a2deb91bd2c8ed1959f0f46cb0c7e08
codepanther <pezechigozirim@gmail.com>
2022-06-08 18:54:05 +0100

    use useNavigate to go to checkout page

--- src/components/CartDropdown/CartDropdown.js
+++ src/components/CartDropdown/CartDropdown.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { CartContext } from '../../contexts/cart.context';
 import Button from '../Button/Button';
 import CartItem from '../CartItem/CartItem';
@@ -7,6 +7,10 @@ import './CartDropdown.styles.scss';
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const goToCheckoutHandler = () => navigate('checkout');
+
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
@@ -16,7 +20,7 @@ const CartDropdown = () => {
           <span className="empty-message">Your cart is empty</span>
         )}
       </div>
-      <Button type="button"><Link to="/checkout">CHECKOUT</Link></Button>
+      <Button type="button" handleButtonClick={goToCheckoutHandler}>CHECKOUT</Button>
     </div>
   );
 };
