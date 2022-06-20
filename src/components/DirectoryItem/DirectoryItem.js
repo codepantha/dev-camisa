9db391c2b498d91e7b30c0ff181c28f05cd77860
codepanther <pezechigozirim@gmail.com>
2022-06-20 19:08:36 +0100

    add navigate to category feature from home page when a directory item is clicked

--- src/components/DirectoryItem/DirectoryItem.js
+++ src/components/DirectoryItem/DirectoryItem.js
@@ -1,16 +1,23 @@
 import PropTypes from 'prop-types';
-// import './DirectoryItem.styles.jsx';
+import { useNavigate } from 'react-router-dom';
+
 import { DirectoryItemContainer, Body, BackgroundImage } from './DirectoryItem.styles';
 
-const DirectoryItem = ({ category: { title, imageUrl } }) => (
-  <DirectoryItemContainer>
-    <BackgroundImage imageUrl={imageUrl} />
-    <Body>
-      <h1>{title}</h1>
-      <p>Shop Now</p>
-    </Body>
-  </DirectoryItemContainer>
-);
+const DirectoryItem = ({ category: { title, imageUrl, route } }) => {
+  const navigate = useNavigate();
+
+  const onNavigateHandler = () => navigate(route);
+
+  return (
+    <DirectoryItemContainer onClick={onNavigateHandler}>
+      <BackgroundImage imageUrl={imageUrl} />
+      <Body>
+        <h1>{title}</h1>
+        <p>Shop Now</p>
+      </Body>
+    </DirectoryItemContainer>
+  );
+};
 
 DirectoryItem.propTypes = {
   category: PropTypes.instanceOf(Object).isRequired,
