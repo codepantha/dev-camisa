06136aaaf7a0e57ec6a147caeede44e5afb52636
codepanther <pezechigozirim@gmail.com>
2022-08-13 06:26:36 +0100

    prevent middleware logging in production

--- src/store/store.js
+++ src/store/store.js
@@ -14,10 +14,24 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middlewares = [logger];
+// Don't log middleware when in production
+const middlewares = [process.env.NODE_ENV !== 'production' && logger].filter(
+  Boolean,
+);
 
-const composedEnhancers = compose(applyMiddleware(...middlewares));
+// if we're not in production, and window object exists,
+// then use the redux_devtools extension
+const composedEnhancer = (process.env.NODE_ENV !== 'production'
+    && window
+    && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__)
+  || compose;
 
-export const store = createStore(persistedReducer, undefined, composedEnhancers);
+const composedEnhancers = composedEnhancer(applyMiddleware(...middlewares));
+
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers,
+);
 
 export const persistor = persistStore(store);
